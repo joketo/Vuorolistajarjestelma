5b222c57f2510a33ff52b9cdf1e5969194e7f812
Tuukka Ylönen <tuukka.ylonen@gmail.com>
2013-05-24 18:16:02 +0300

    ensimmäinen prototyyppi sisäänkirjautumisjärjestelmästä

--- auth.py
+++ auth.py
@@ -1,5 +1,42 @@
 import bcrypt
 
+class User(object):
+    def __init__(self, name, passwd, sid=None):
+        """User class, params:
+        name: user name
+        salt: salt for password hash :: builtins.bytes
+        pwhash: bcrypt.hashpw(salt, password) :: builtins.bytes
+        sid: session id
+        """
+
+        self.name = name
+        self.salt = bcrypt.gensalt()
+        self.pwhash = bcrypt.hashpw(passwd.encode(), self.salt)
+        self.sid = sid
+
 class Auth(object):
     """Module for authenticating users and managing sessions."""
+    def __init__(self):
+        #test users-dict
+        self.users = {"pekka":User("pekka", "salasana")}
+
+    def login(self, uname, passwd):
+        """Logs a user in, sets and returns a session id"""
+        user = self.users[uname] #TODO: catch this
+        if user.pwhash == bcrypt.hashpw(passwd, user.salt):
+            user.sid = 1 #TODO: something sane here
+            return user.sid
+        
+    def isLogged(self, uname, sid):
+        return sid == self.users[uname].sid
+
+    def logout(self, uname):
+        self.users[uname].sid = None
+
+    def register(self, uname, passwd):
+        if not uname in self.users:
+            newUser = User(uname, passwd)
     
+    
+
+
