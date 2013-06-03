f8d239e9c9c2110b1d963381119c8917275b98b0
Tuukka Ylönen <tuukka.ylonen@gmail.com>
2013-06-03 15:58:23 +0300

    refaktoroitu create_tablesia ja lisätty hoitsutaulun luonti

--- create_tables.py
+++ create_tables.py
@@ -1,12 +1,21 @@
 import sqlite3
 
-def create_tables():
-    conn = sqlite3.connect("test.db")
+def usedb(dbname, dbstring, createparams=tuple()):
+    conn = sqlite3.connect(dbname)
     c = conn.cursor()
-    c.execute("""CREATE TABLE users
-              (id integer primary key autoincrement, username text UNIQUE, 
-              salt blob, hash blob)""")
+    c.execute(requeststring, createparams)
     conn.commit()
     conn.close()
 
-create_tables()
+def create_users(dbname):
+    accessdb("""CREATE TABLE users
+                (id integer primary key autoincrement, username text UNIQUE, 
+                 salt blob, hash blob)""")
+
+def create_hoitsut(dbname):
+    usedb("""Create TABLE hoitajat
+             (id integer primary key autoincrement, name text UNIQUE,
+              perms blob)""")
+
+create_users("test.db")
+create_hoitsut("test.db")
